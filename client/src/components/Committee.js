2207eec97566bafdc02dbc34d81e62ed52373d7a
jeet <gjeet7415@gmail.com>
2025-02-08 23:27:32 +0530

    1/committee.js

--- client/src/components/Committee.js
+++ client/src/components/Committee.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ComingSoon from './ComingSoon';
+import CommitteeDetail from './CommitteeDetail';
+import Home from './Home';
+import Title from './Title';
 
 function Committee() {
-  return <ComingSoon pageName="Committee" />;
+
+  return (
+    <div>
+      <Home/>
+      <Title titleName= 'Conference Committee Members'/>
+      <CommitteeDetail MemberPost={'Cheif Patron'} MemberName={'DR B.K Sethi'} MemberPostCollege={'Assistant Proffesor, SVNIT, Surat'}/>
+      <CommitteeDetail MemberPost={'Cheif Patron'} MemberName={'DR B.K Sethi'} MemberPostCollege={'Assistant Proffesor, SVNIT, Surat'}/>
+      <CommitteeDetail MemberPost={'Cheif Patron'} MemberName={'DR B.K Sethi'} MemberPostCollege={'Assistant Proffesor, SVNIT, Surat'}/>
+      <CommitteeDetail MemberPost={'Cheif Patron'} MemberName={'DR B.K Sethi'} MemberPostCollege={'Assistant Proffesor, SVNIT, Surat'}/>
+    </div>
+  )
 }
 
 export default Committee; 
